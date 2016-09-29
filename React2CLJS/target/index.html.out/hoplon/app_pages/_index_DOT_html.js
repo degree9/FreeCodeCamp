d28c24889cefb2795dc243d39df317c9b216b05a
Matthew Ratzke <matt@degree9.io>
2016-09-29 14:36:57 -0600

    Add HTML HEAD BODY elements

--- React2CLJS/target/index.html.out/hoplon/app_pages/_index_DOT_html.js
+++ React2CLJS/target/index.html.out/hoplon/app_pages/_index_DOT_html.js
@@ -4,5 +4,6 @@ goog.require('cljs.core');
 goog.require('javelin.core');
 goog.require('hoplon.jquery');
 goog.require('hoplon.core');
+hoplon.core.html.call(null,hoplon.core.head.call(null),hoplon.core.body.call(null));
 
 //# sourceMappingURL=_index_DOT_html.js.map
