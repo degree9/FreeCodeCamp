6739f32848a625c43d8b13670757a927ca77d0dc
Matthew Ratzke <matt@degree9.io>
2016-09-29 14:43:29 -0600

    Add HEAD content
    NOTE: meta translates to html-meta do to a conflict with
    clojure.core/meta

--- React2CLJS/target/index.html.out/hoplon/app_pages/_index_DOT_html.js
+++ React2CLJS/target/index.html.out/hoplon/app_pages/_index_DOT_html.js
@@ -4,6 +4,6 @@ goog.require('cljs.core');
 goog.require('javelin.core');
 goog.require('hoplon.jquery');
 goog.require('hoplon.core');
-hoplon.core.html.call(null,hoplon.core.head.call(null),hoplon.core.body.call(null));
+hoplon.core.html.call(null,hoplon.core.head.call(null,hoplon.core.html_meta.call(null,new cljs.core.Keyword(null,"charset","charset",-1063822193),"utf8"),hoplon.core.html_meta.call(null,new cljs.core.Keyword(null,"name","name",1843675177),"viewport",new cljs.core.Keyword(null,"content","content",15833224),"width=device-width, initial-scale=1"),hoplon.core.link.call(null,new cljs.core.Keyword(null,"rel","rel",1378823488),"shortcut icon",new cljs.core.Keyword(null,"href","href",-793805698),"favicon.ico"),hoplon.core.title.call(null,"React2Cljs App")),hoplon.core.body.call(null));
 
 //# sourceMappingURL=_index_DOT_html.js.map
